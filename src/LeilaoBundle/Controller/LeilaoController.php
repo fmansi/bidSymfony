626df7b835d8858f2a92a40c7a9dd5076e979af5
dereck leme <dereckleme@globo.com>
2016-08-22 21:53:08 -0300

    implementação

--- src/LeilaoBundle/Controller/LeilaoController.php
+++ src/LeilaoBundle/Controller/LeilaoController.php
@@ -5,6 +5,7 @@ namespace LeilaoBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Leilao;
 
 class LeilaoController extends Controller
 {
@@ -12,10 +13,14 @@ class LeilaoController extends Controller
     {
         $serviceLeilao = $this->get("leilao.Service");
 
-        $todosLeiloes = $serviceLeilao->getList($situation);
+        $leiloesAbertos = $serviceLeilao->getList(Leilao::ABERTO);
+        $proximosLeiloes = $serviceLeilao->getList(Leilao::PROXIMOS);
+        $fechadosLeiloes = $serviceLeilao->getList(Leilao::FECHADO);
 
         return $this->render('LeilaoBundle:leilao:index.html.twig', array(
-            "listaLeilao" => $todosLeiloes
+            "leiloesAbertos" => $leiloesAbertos,
+            "proximosLeiloes" => $proximosLeiloes,
+            "fechadosLeiloes" => $fechadosLeiloes
         ));
     }
 }
