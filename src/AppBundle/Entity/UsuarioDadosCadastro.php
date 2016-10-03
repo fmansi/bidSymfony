6c64c649b693de5f240750307631902a703be352
Dereck Vicentin Leme <dereckleme@globo.com>
2016-10-02 22:56:24 -0300

    bug

--- src/AppBundle/Entity/UsuarioDadosCadastro.php
+++ src/AppBundle/Entity/UsuarioDadosCadastro.php
@@ -315,7 +315,7 @@ class UsuarioDadosCadastro
      */
     public function getDataNascimento()
     {
-        return $this->dataNascimento->format('d/m/y');
+        return $this->dataNascimento->format('d/m/Y');
     }
 
     /**
